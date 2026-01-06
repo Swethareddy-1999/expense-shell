c57928d42d72b4735f50984bbd2b987d15704098
swetha <swethareddy01.com>
2026-01-06 12:34:12 +0530

    expense shell

---
+++ frontend.sh
@@ -0,0 +1,51 @@
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+y="\e[31m"
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$( echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ] #$?privious output will come here as input
+    then    
+        echo "$2 ...$R failure"
+        exit 1
+    else
+        echo "$2 ... $G success"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ] #ne=not equal
+    then 
+        echo "ERROR:: you must have sudo access to excute this script"
+        exit 1 #other then 0
+    fi
+}
+
+echo "Script started excuting at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf install nginx -y &>>$LOG_FILE_NAME
+VALIDATE $? " installing nginx "
+
+systemctl enable nginx &>>$LOG_FILE_NAME
+VALIDATE $? " enabling nginx "
+
+systemctl start nginx &>>$LOG_FILE_NAME
+VALIDATE $? " starting nginx "
+
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+
+cd /usr/share/nginx/html
+rm -rf /usr/share/nginx/html/*
+
+unzip /tmp/frontend.zip
+
+cp /home/ec2-user/expense-shell /etc/nginx/default.d/expense.conf &>>$LOG_FILE_NAME
